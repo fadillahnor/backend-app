144801428649a265413b7bdd5e374aa1ec363878
fadillah <fdillah155@gmail.com>
2026-07-15 20:56:05 +0700

    Auto-create database tables on startup

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import create_app
+from app.ext import db
 
 app = create_app()
+
+# Auto-create all tables in database on startup
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(
